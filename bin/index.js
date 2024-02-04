8aa46a763759ec5772c7ec5bafe905827817adae
Kai <thienphuc.bka1412@gmail.com>
2024-02-04 14:40:33 +0700

    add to change script

--- bin/index.js
+++ bin/index.js
@@ -1,2 +1,18 @@
 #!/usr/bin/env node
 
+import tagComponents from "../commands/tagComponents.js";
+
+const action = process.argv[2];
+
+switch (action) {
+  case "tag":
+    tagComponents();
+    break;
+
+  // case "export":
+  //   deleteFileOrFolder(params);
+  //   break;
+
+  default:
+    break;
+}
